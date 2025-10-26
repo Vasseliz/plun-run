409e8b767c293b3cc4cae73bda10a9d50202f5f6
Vasseliz <nicolaszielasko.com>
2025-10-26 09:39:31 -0300

    corrigindo para uma importação mais moderna

--- src/app/services/gemini-api.service.ts
+++ src/app/services/gemini-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError, catchError, map } from 'rxjs';
 import { Training, TrainingGoal } from '../models/training.model';
 import { environment } from 'src/environments/environment';
 
